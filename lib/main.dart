438b5eacbf5dc89c7d22de0469a5b2e5d53167db
Fran <hola@beachlab.org>
2019-12-30 02:00:45 +0100

    8 notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:Xylophone/xilo.dart';
 
 void main() {
+  // Set the device orientation and wait to start the app
+//  SystemChrome.setPreferredOrientations(
+//      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then(
+//    (_) {
   runApp(XylophoneApp());
+//    },
+//  );
 }
